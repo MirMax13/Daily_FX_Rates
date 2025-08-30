6de9a644c2d8457eb6add35204cfb0d7871d961e
Maxym Matskiv <maksymvolodymyr.matskiv@lnu.edu.ua>
2025-08-30 16:58:01 +0300

    Implemented code to see database table contents

---
+++ database.py
@@ -0,0 +1,23 @@
+import sqlite3
+
+conn = sqlite3.connect('fx_rates.db')
+cursor = conn.cursor()
+
+query = "SELECT * FROM observations"
+
+cursor.execute(query)
+rows = cursor.fetchall()
+
+for row in rows:
+    print(row)
+
+query = "SELECT * FROM series"
+
+cursor.execute(query)
+rows = cursor.fetchall()
+
+for row in rows:
+    print(row)
+
+# Close the database connection
+conn.close()
