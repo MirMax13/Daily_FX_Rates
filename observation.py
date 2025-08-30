07cc232e639baa07ba06151e71f37a792da1f337
Maxym Matskiv <maksymvolodymyr.matskiv@lnu.edu.ua>
2025-08-30 16:27:43 +0300

    Refactor code to handle Error 429: Too Many Requests

--- observation.py
+++ observation.py
@@ -1,32 +1,38 @@
 ########### Python 3.2 #############
-import urllib.request, json
+import urllib.request, json,time
 
-def fetch_and_save_latest_observation(series_id, cursor, conn):
-    try:
-        url = f"https://api.riksbank.se/swea/v1/Observations/Latest/{series_id[0]}"
+def fetch_and_save_latest_observation(series_id, cursor, conn, retries=5, delay=15):
+    url = f"https://api.riksbank.se/swea/v1/Observations/Latest/{series_id[0]}"
+    hdr = {
+        # Request headers
+        'Cache-Control': 'no-cache',
+    }
+    for attempt in range(retries):
+        try:
+            req = urllib.request.Request(url, headers=hdr)
 
-        hdr = {
-            # Request headers
-            'Cache-Control': 'no-cache',
-        }
+            req.get_method = lambda: 'GET'
+            response = urllib.request.urlopen(req)
+            if response.getcode() != 200:
+                raise Exception(f"HTTP {response.getcode()}")
+            # Save results into observations table
+            data = json.loads(response.read())
+            
+            cursor.execute('''
+                    INSERT OR IGNORE INTO observations (seriesId, date, value)
+                    VALUES (?, ?, ?)
+                ''', (
+                    series_id[0],
+                    data['date'],
+                    data['value']
+                ))
+            conn.commit()
 
-        req = urllib.request.Request(url, headers=hdr)
-
-        req.get_method = lambda: 'GET'
-        response = urllib.request.urlopen(req)
-        # Save results into observations table
-        data = json.loads(response.read())
-        cursor.execute('''
-                INSERT OR IGNORE INTO observations (seriesId, date, value)
-                VALUES (?, ?, ?)
-            ''', (
-                series_id[0],
-                data['date'],
-                data['value']
-            ))
-        conn.commit()
-
-        print(data)
-    except Exception as e:
-        print(e)
+            print(data)
+        except Exception as e:
+            print(e)
+            if "429" in str(e):
+                print(f"❌ {series_id[0]}: Rate limit exceeded, retrying in {delay} seconds...")
+                time.sleep(delay)
+                
 ####################################
